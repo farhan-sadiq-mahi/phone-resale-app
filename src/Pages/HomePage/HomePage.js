d02312eb5fcbe69cc92a3ea4ab62846293b8e4e3
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-24 09:12:45 +0600

    feature section added

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FeatureComponent from './FeatureComponent';
 import HeroComponent from './HeroComponent';
 
 const HomePage = () => {
     return (
         <>
             <HeroComponent />
+            <FeatureComponent />
         </>
     );
 };
