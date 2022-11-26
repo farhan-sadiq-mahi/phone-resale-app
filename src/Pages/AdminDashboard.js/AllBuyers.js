e7026a6d3884821f195913e460b7c1f43f317444
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-26 22:34:03 +0600

    all seller and buyers data fetched and added

--- src/Pages/AdminDashboard.js/AllBuyers.js
+++ src/Pages/AdminDashboard.js/AllBuyers.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import Loader from '../../SharedComponent/Loader';
 
 const AllBuyers = () => {
+    const { user } = useContext(AuthContext);
+    const { data, isLoading } = useQuery({
+        queryKey: ['buyers'],
+        queryFn: () => fetch(`http://localhost:5000/getallbuyers?email=${user.email}`)
+            .then(res => res.json())
+    })
+    if (isLoading) {
+        return <Loader />
+    }
     return (
-        <div>
-            all buyers are here
+        <div className='container'>
+            <h1 className='text-4xl font-bold merFont text-center my-5'>All Sellers</h1>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th></th>
+                            <th>Action</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            data.map(buyer =>
+                                <tr key={buyer._id}>
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={buyer.img} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{buyer.name}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {buyer.email}
+                                    </td>
+                                    <td></td>
+                                    <th>
+                                        <button className="btn btn-error btn-xs">Delete</button>
+                                    </th>
+                                </tr>)
+                        }
+
+                    </tbody>
+                    {/* <!-- foot --> */}
+                    <tfoot>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th></th>
+                            <th>Action</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/AdminDashboard.js/AllSellers.js
+++ src/Pages/AdminDashboard.js/AllSellers.js
@@ -1,9 +1,80 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import Loader from '../../SharedComponent/Loader';
 
 const AllSellers = () => {
+    const { user } = useContext(AuthContext);
+    const { data, isLoading } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: () => fetch(`http://localhost:5000/getallsellers?email=${user.email}`)
+            .then(res => res.json())
+    })
+    if (isLoading) {
+        return <Loader />
+    }
+
+    console.log(data);
     return (
-        <div>
-            ALl sellers are here
+        <div className='container'>
+            <h1 className='text-4xl font-bold merFont text-center my-5'>All Sellers</h1>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Status</th>
+                            <th></th>
+                            <th>Action</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            data.map(seller =>
+                                <tr key={seller._id}>
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={seller.img} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{seller.name}</div>
+                                                <div className="text-sm opacity-50">{seller.email}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {
+                                            seller?.isVerified ? <button className='btn btn-success btn-xs'>Verified</button> :
+                                                <button className='btn btn-info btn-xs'>Verify Now</button>
+                                        }
+                                    </td>
+                                    <td></td>
+                                    <th>
+                                        <button className="btn btn-error btn-xs">Delete</button>
+                                    </th>
+                                </tr>)
+                        }
+
+                    </tbody>
+                    {/* <!-- foot --> */}
+                    <tfoot>
+                        <tr>
+                            <th>Name</th>
+                            <th>Status</th>
+                            <th></th>
+                            <th>Action</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
 };
